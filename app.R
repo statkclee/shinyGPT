35b294d67c39c8cbefd6649c60e9a20e1f2c08c9
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2023-08-07 23:35:22 +0900

    start drawing

--- app.R
+++ app.R
@@ -1,173 +1,61 @@
-# https://www.listendata.com/2023/04/how-to-build-chatgpt-clone-in-shiny.html
-
 library(shiny)
-library(httr)
-library(sass)
-library(markdown)
-library(waiter)
-library(shinyjs)
-library(shinyCopy2clipboard)
-# remotes::install_github("deepanshu88/shinyCopy2clipboard")
 
-css <- sass(sass_file("www/chat.scss"))
-
-jscode <- 'var container = document.getElementById("chat-container");
-if (container) {
-  var elements = container.getElementsByClassName("user-message");
-  if (elements.length > 1) {
-    var lastElement = elements[elements.length - 1];
-    lastElement.scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-}'
-
-chatGPT_R <- function(apiKey, prompt, model="gpt-3.5-turbo") {
-  response <- POST(
-    url = "https://api.openai.com/v1/chat/completions",
-    add_headers(Authorization = paste("Bearer", apiKey)),
-    content_type("application/json"),
-    encode = "json",
-    body = list(
-      model = model,
-      messages = list(
-        list(role = "user", content = prompt)
-      )
-    )
-  )
-  
-  if(status_code(response)>200) {
-    result <- trimws(content(response)$error$message)
-  } else {
-    result <- trimws(content(response)$choices[[1]]$message$content)
-  }
-  
-  return(result)
-  
-}
-
-execute_at_next_input <- function(expr, session = getDefaultReactiveDomain()) {
-  observeEvent(once = TRUE, reactiveValuesToList(session$input), {
-    force(expr)
-  }, ignoreInit = TRUE)
-}
-
-# Define UI for application
 ui <- fluidPage(
-  useWaiter(),
-  useShinyjs(),
-  use_copy(),
-  tags$head(tags$style(css)),
+  # Define the side panel
   sidebarLayout(
     sidebarPanel(
-      textInput("apiKey", "API Key", "sk-xxxxxxxxxxxxxxxxxxxx"),
-      selectInput("model", "OpenAI LLM 모형", choices = c("gpt-3.5-turbo", "gpt-4"), selected = "gpt-3.5-turbo"),
-      style = "background-color: #fff; color: #333; border: 1px solid #ccc;"
+      # Dropdown 1
+      selectInput("dropdown1", 
+                  label = "Dropdown Menu 1", 
+                  choices = c("Option 1.1", "Option 1.2", "Option 1.3")),
+      
+      # Dropdown 2
+      selectInput("dropdown2", 
+                  label = "Dropdown Menu 2", 
+                  choices = c("Option 2.1", "Option 2.2", "Option 2.3")),
+      
+      # Text input for image description
+      textInput("imagePrompt", "Enter image description:", 
+                "blue sky and many pine trees with snowy mountains"),
+      
+      # Action button to trigger draw_img function
+      actionButton("drawButton", "Draw Image")
     ),
     
+    # Define the main panel
     mainPanel(
-      tags$div(
-        id = "chat-container",
-        tags$div(
-          id = "chat-header",
-          tags$img(src = "kwangmyung.svg", alt = "AI 사진"),
-          tags$h3("광명시 챗봇")
-        ),
-        tags$div(
-          id = "chat-history",
-          uiOutput("chatThread"),
-        ),
-        tags$div(
-          id = "chat-input",
-          tags$form(
-            column(12,textAreaInput(inputId = "prompt", label="", placeholder = "프롬프트를 여기에 작성해주세요...", width = "100%")),
-            fluidRow(
-              tags$div(style = "margin-left: 1.5em;",
-                       actionButton(inputId = "submit",
-                                    label = "프롬프트 보내기",
-                                    icon = icon("paper-plane")),
-                       actionButton(inputId = "remove_chatThread",
-                                    label = "기록 삭제",
-                                    icon = icon("trash-can")),
-                       CopyButton("clipbtn",
-                                  label = "복사",
-                                  icon = icon("clipboard"),
-                                  text = "")
-                       
-              ))
-          ))
-      )
-    ))
+      # Display the PNG image using uiOutput
+      uiOutput("displayImageUI")
+    )
+  )
 )
 
-# Define server logic
-server <- function(input, output, session) {
+server <- function(input, output) {
   
-  historyALL <- reactiveValues(df = data.frame() , val = character(0))
-  
-  # On click of send button
-  observeEvent(input$submit, {
+  observeEvent(input$drawButton, {
+    draw_img(
+      input$imagePrompt,
+      ko2en = TRUE,
+      n = 1L,
+      size = "512x512",
+      type = "file",
+      format = "png",
+      path = "./www/",
+      fname = "shiny_prompt3",
+      openai_api_key = Sys.getenv("OPENAI_API_KEY")
+    )
     
-    if (nchar(trimws(input$prompt)) > 0) {
-      
-      # Spinner
-      w <- Waiter$new(id = "chat-history",
-                      html = spin_3(),
-                      color = transparent(.5))
-      w$show()
-      
-      # Response
-      chatGPT <- chatGPT_R(input$apiKey, input$prompt, input$model)
-      historyALL$val <- chatGPT
-      history <- data.frame(users = c("Human", "AI"),
-                            content = c(input$prompt, markdown::mark_html(text=chatGPT)),
-                            stringsAsFactors = FALSE)
-      historyALL$df <- rbind(historyALL$df, history)
-      updateTextInput(session, "prompt", value = "")
-      
-      # Conversation Interface
-      output$chatThread <- renderUI({
-        conversations <- lapply(seq_len(nrow(historyALL$df)), function(x) {
-          tags$div(class = ifelse(historyALL$df[x, "users"] == "Human",
-                                  "user-message",
-                                  "bot-message"),
-                   HTML(paste0(ifelse(historyALL$df[x, "users"] == "Human",
-                                      "
-<img src='question.svg' class='img-wrapper2'>
-",
-"
-<img src='answer.svg' class='img-wrapper2'>
-"),
-historyALL$df[x, "content"])))
-        })
-        do.call(tagList, conversations)
-      })
-      
-      w$hide()
-      execute_at_next_input(runjs(jscode))
-      
-    }
+    # Display the image
+    output$displayImageUI <- renderUI({
+      tags$img(src = "./shiny_prompt3.png", 
+               width = "512px", 
+               height = "512px", 
+               alt = "Generated Image")
+    })
+    
     
   })
-  
-  observeEvent(input$remove_chatThread, {
-    output$chatThread <- renderUI({return(NULL)})
-    historyALL$df <- NULL
-    updateTextInput(session, "prompt", value = "")
-  })
-  
-  observe({
-    req(input$clipbtn)
-    CopyButtonUpdate(session,
-                     id = "clipbtn",
-                     label = "복사",
-                     icon = icon("clipboard"),
-                     text = as.character(historyALL$val))
-    
-  })
-  
-  
 }
 
-# Run the application
-shinyApp(ui=ui, server=server)
+# Run the application 
+shinyApp(ui = ui, server = server)
